189ece8597a19aa972b9de2011d59220f1cf7b37
Charles <charles.hebert1995@gmail.com>
2019-04-04 17:11:50 -0400

    first commit

---
+++ Resources/BatteryResource.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Supermarket.API.Resources
+{
+    public class SaveBatteryResource
+    {
+        [Required]
+        [MaxLength(30)]
+        // public string Name { get; set; }
+
+        public int id { get; set; }
+        public int building_id { get; set; }
+        public string status { get; set; }
+        public BatteryResource Battery {get;set;}
+    }
+}
+
+
+namespace Supermarket.API.Resources
+{
+    public class BatteryResource
+    {
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+
+        public int id { get; set; }
+        public int building_id { get; set; }
+        public string status { get; set; }
+    }
+}
+
+// namespace Supermarket.API.Resources
+// {
+//      public class ProductResource
+//     {
+//         public int id { get; set; }
+//         public int building_id { get; set; }
+//         public string status { get; set; }
+//         public BatteryResource Battery {get;set;}
+//     }
+
+// }
